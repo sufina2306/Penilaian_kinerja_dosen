b1280ec58fc132de7a1b12402329f18f44e3c29d
mujaddid <mujaddidihsan298@gmail.com>
2025-01-15 05:28:00 +0700

    web: rencana

---
+++ resources/views/pages/dosen/rencana.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app_dosen')
+
+@section('title', 'Rencana')
+
+@section('content')
+    <div class="row">
+
+        <h1>Halaman Rencana</h1>
+      
+
+
+ 
+    </div>
+@endsection
